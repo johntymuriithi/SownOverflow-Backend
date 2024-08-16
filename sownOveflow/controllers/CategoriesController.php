3ab159348f68919712ff3734bdbaaf6f455ac4e7
johntymuriithi <muriithijohn634@gmail.com>
2024-08-16 09:39:18 +0300

    updated the category ednpoint

--- sownOveflow/controllers/CategoriesController.php
+++ sownOveflow/controllers/CategoriesController.php
@@ -4,7 +4,6 @@ namespace app\controllers;
 use app\models\Answers;
 
 use app\models\Categories;
-use app\models\Orders;
 use app\models\Questions;
 use Yii;
 use yii\web\NotFoundHttpException;
@@ -35,8 +34,16 @@ class CategoriesController extends BaseController
         Yii::$app->response->format = Response::FORMAT_JSON;
 
         $category = Categories::find()->all();
+        $total = [];
         if ($category) {
-            return $category;
+            foreach($category as $item) {
+                $total[] = [
+                    'category_id' => $item->category_id,
+                    'category_name' => $item->category_name,
+                    'category_label' => $item->category_name,
+                ];
+            }
+            return $total;
         } else {
             throw new NotFoundHttpException("No Categories for now!");
         }
